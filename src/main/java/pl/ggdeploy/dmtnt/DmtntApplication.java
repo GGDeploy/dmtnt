704402f335b0c47e317024394462127e895c8d1f
Enessetere <muszalski.sebastian@gmail.com>
2021-01-31 11:09:37 +0100

    initail commit

---
+++ src/main/java/pl/ggdeploy/dmtnt/DmtntApplication.java
@@ -0,0 +1,13 @@
+package pl.ggdeploy.dmtnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmtntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmtntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ggdeploy/dmtnt/DmtntApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ggdeploy.dmtnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmtntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
